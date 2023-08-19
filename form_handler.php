ca0602deb9931242eb9c681799ab0c9ce3d63886
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-19 05:03:59 -0700

    website updates: changed form styyling and implementation, reduced size of landing page image and changed resume file

---
+++ form_handler.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $email_from = 'form@ehiane.info';
+    $email_subject = 'New Form Submission';
+    $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                  "Subject: $subject.\n".
+                  "User Message: $message .\n";
+
+    $to = "ehis.oigiagbe@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        // Email sent successfully
+        echo "Thank you for your message!";
+    } else {
+        // Email failed to send
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
